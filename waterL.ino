23740960f4715bf7a82b6929169fcc49960220a4
sangeeth shravan s <40892525+Sangeethshravan97@users.noreply.github.com>
2020-02-29 21:38:41 +0530

    Update waterL.ino

--- waterL.ino
+++ waterL.ino
@@ -5,8 +5,8 @@
 #include <FirebaseArduino.h>
 
 
-#define FIREBASE_HOST "iotc-994b6.firebaseio.com"
-#define FIREBASE_AUTH "3deKcbWp2v5e07GGcoYyKPdlWobUQtADpASSFzUx"
+#define FIREBASE_HOST "www.firebaseiso.com" // host name
+#define FIREBASE_AUTH "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" // auth key
 #define WIFI_SSID "Hawkeye"
 #define WIFI_PASSWORD "Human1936"
 
